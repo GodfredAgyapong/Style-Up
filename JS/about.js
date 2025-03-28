a6829b9e24def913e82e84154fac80bc70abde21
GodfredAgyapong <godfred515@gmail.com>
2025-03-28 11:13:57 +0000

    Added the JS respository

---
+++ JS/about.js
@@ -0,0 +1,15 @@
+let x = document.getElementById("btn");
+
+x.style.backgroundColor = "#101C26";
+x.style.color = "white";
+x.style.padding = "11px 20px 13px 20px";
+x.style.border = "none";
+x.style.borderRadius = "20px";
+x.style.cursor = "pointer";
+
+function clickMe() {
+    alert("Men's fashion at it's finest")
+}
+
+
+
